ccc5aaed8b9bfcb3eb3e92d8d9a7f6e0e68ec4fa
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-05-29 10:20:42 +0200

    make accessor mutable in case of java 11+

--- src/main/java/fuzs/diagonalfences/mixin/accessor/IStateHolderAccessor.java
+++ src/main/java/fuzs/diagonalfences/mixin/accessor/IStateHolderAccessor.java
@@ -3,12 +3,14 @@ package fuzs.diagonalfences.mixin.accessor;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.state.StateHolder;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(StateHolder.class)
 public interface IStateHolderAccessor<O, S> {
 
     @Accessor("field_235893_d_")
+    @Mutable
     void setCodec(MapCodec<S> codec);
 
 }
