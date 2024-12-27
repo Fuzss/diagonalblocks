451a03307d79b15d9432f67fd88f5f4986597dfb
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-28 00:12:47 +0100

    Update DiagonalBlockTypes.java

--- 1.21.3/Common/src/main/java/fuzs/diagonalblocks/api/v2/DiagonalBlockTypes.java
+++ 1.21.3/Common/src/main/java/fuzs/diagonalblocks/api/v2/DiagonalBlockTypes.java
@@ -10,24 +10,36 @@ import java.util.List;
 import java.util.function.Function;
 
 public final class DiagonalBlockTypes {
-    public static final DiagonalBlockType FENCE = new DiagonalBlockTypeImpl("fences", FenceBlock.class,
-            t -> DiagonalFenceBlock::new, CrossCollisionBlock.NORTH, CrossCollisionBlock.EAST, CrossCollisionBlock.WEST,
-            CrossCollisionBlock.SOUTH, CrossCollisionBlock.WATERLOGGED
-    );
-    public static final DiagonalBlockType WINDOW = new DiagonalBlockTypeImpl("windows", IronBarsBlock.class,
-            DiagonalBlockTypes::getDiagonalGlassPaneFactory, CrossCollisionBlock.NORTH, CrossCollisionBlock.EAST,
-            CrossCollisionBlock.WEST, CrossCollisionBlock.SOUTH, CrossCollisionBlock.WATERLOGGED
-    );
-    public static final DiagonalBlockType WALL = new DiagonalBlockTypeImpl("walls", WallBlock.class,
-            t -> DiagonalWallBlock::new, WallBlock.UP, WallBlock.NORTH_WALL, WallBlock.EAST_WALL, WallBlock.WEST_WALL,
-            WallBlock.SOUTH_WALL, WallBlock.WATERLOGGED
-    );
+    public static final DiagonalBlockType FENCE = new DiagonalBlockTypeImpl("fences",
+            FenceBlock.class,
+            (Block block) -> DiagonalFenceBlock::new,
+            CrossCollisionBlock.NORTH,
+            CrossCollisionBlock.EAST,
+            CrossCollisionBlock.WEST,
+            CrossCollisionBlock.SOUTH,
+            CrossCollisionBlock.WATERLOGGED);
+    public static final DiagonalBlockType WINDOW = new DiagonalBlockTypeImpl("windows",
+            IronBarsBlock.class,
+            DiagonalBlockTypes::getDiagonalGlassPaneFactory,
+            CrossCollisionBlock.NORTH,
+            CrossCollisionBlock.EAST,
+            CrossCollisionBlock.WEST,
+            CrossCollisionBlock.SOUTH,
+            CrossCollisionBlock.WATERLOGGED);
+    public static final DiagonalBlockType WALL = new DiagonalBlockTypeImpl("walls",
+            WallBlock.class,
+            (Block block) -> DiagonalWallBlock::new,
+            WallBlock.UP,
+            WallBlock.NORTH_WALL,
+            WallBlock.EAST_WALL,
+            WallBlock.WEST_WALL,
+            WallBlock.SOUTH_WALL,
+            WallBlock.WATERLOGGED);
 
     static {
-        ModBlockTagsProvider.BUILT_IN_BLACKLISTED_TYPES.forEach(
-                (DiagonalBlockType diagonalBlockType, List<String> strings) -> {
-                    strings.stream().map(ResourceLocationHelper::parse).forEach(diagonalBlockType::disableBlockFactory);
-                });
+        ModBlockTagsProvider.BUILT_IN_BLACKLISTED_TYPES.forEach((DiagonalBlockType diagonalBlockType, List<String> strings) -> {
+            strings.stream().map(ResourceLocationHelper::parse).forEach(diagonalBlockType::disableBlockFactory);
+        });
     }
 
     private DiagonalBlockTypes() {
