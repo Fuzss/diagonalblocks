3c41d03c85f745c47440b74586e7eb6b3f3aa154
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-06-20 09:02:53 +0200

    prepare 1.21.6 port

---
+++ 1.21.6/Common/src/main/java/fuzs/diagonalblocks/client/resources/translator/WallMultiPartTranslator.java
@@ -0,0 +1,64 @@
+package fuzs.diagonalblocks.client.resources.translator;
+
+import com.google.common.collect.ImmutableList;
+import fuzs.diagonalblocks.api.v2.DiagonalBlockTypes;
+import fuzs.diagonalblocks.api.v2.client.MultiPartTranslator;
+import fuzs.diagonalblocks.client.resources.model.ConditionHelper;
+import net.minecraft.client.renderer.block.model.BlockModelDefinition;
+import net.minecraft.client.renderer.block.model.multipart.Condition;
+import net.minecraft.client.renderer.block.model.multipart.Selector;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.WallSide;
+
+import java.util.Optional;
+import java.util.function.UnaryOperator;
+
+public final class WallMultiPartTranslator extends MultiPartTranslator {
+
+    public WallMultiPartTranslator() {
+        super(DiagonalBlockTypes.WALL);
+    }
+
+    @Override
+    protected Comparable<?> getNewPropertyValue(Property<?> oldProperty, Property<?> newProperty, Comparable<?> oldValue) {
+        if (newProperty.getValueClass() == WallSide.class && oldValue instanceof Boolean) {
+            return (Boolean) oldValue ? WallSide.LOW : WallSide.NONE;
+        } else {
+            return super.getNewPropertyValue(oldProperty, newProperty, oldValue);
+        }
+    }
+
+    @Override
+    protected BlockModelDefinition.MultiPartDefinition getModelFromBase(BlockModelDefinition.MultiPartDefinition multiPart) {
+        ImmutableList.Builder<Selector> builder = ImmutableList.builder();
+        for (Selector selector : multiPart.selectors()) {
+            if (selector.condition().isEmpty()) {
+                builder.add(selector);
+            } else {
+                Condition condition = ConditionHelper.deepCopy(selector.condition().get(),
+                        UnaryOperator.identity(),
+                        (String key, String value) -> {
+                            if (value.equals(WallSide.LOW.toString())) {
+                                return Boolean.TRUE.toString();
+                            } else if (value.equals(WallSide.NONE.toString())) {
+                                return Boolean.FALSE.toString();
+                            } else if (value.equals(WallSide.TALL.toString())) {
+                                return null;
+                            } else {
+                                return value;
+                            }
+                        });
+                if (condition != null) {
+                    builder.add(new Selector(Optional.of(condition), selector.variant()));
+                }
+            }
+        }
+        return new BlockModelDefinition.MultiPartDefinition(builder.build());
+    }
+
+    @Override
+    public boolean allowBaseModelAsFallback() {
+        // since we change the block states on our wall block implementation from vanilla it is not safe to use the model of the vanilla wall base block
+        return false;
+    }
+}
---
+++ 1.21.6/Fabric/src/main/java/fuzs/diagonalblocks/fabric/mixin/client/ParticleEngineFabricMixin.java
@@ -0,0 +1,28 @@
+package fuzs.diagonalblocks.fabric.mixin.client;
+
+import fuzs.diagonalblocks.client.util.DestroyEffectsHelper;
+import fuzs.diagonalblocks.api.v2.impl.StarShapeProvider;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ParticleEngine.class)
+abstract class ParticleEngineFabricMixin {
+    @Shadow
+    protected ClientLevel level;
+
+    @Inject(method = "destroy", at = @At("HEAD"), cancellable = true)
+    public void destroy(BlockPos blockPos, BlockState blockState, CallbackInfo callback) {
+        if (blockState.getBlock() instanceof StarShapeProvider) {
+            if (DestroyEffectsHelper.addDestroyEffects(blockState, this.level, blockPos, ParticleEngine.class.cast(this))) {
+                callback.cancel();
+            }
+        }
+    }
+}
