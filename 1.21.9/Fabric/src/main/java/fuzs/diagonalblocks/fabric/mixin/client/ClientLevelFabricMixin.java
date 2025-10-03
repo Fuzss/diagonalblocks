b7978172960b71e873de58dbe7dde7264a36e43a
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-03 20:29:27 +0200

    release v21.9.0

---
+++ 1.21.9/Fabric/src/main/java/fuzs/diagonalblocks/fabric/mixin/client/ClientLevelFabricMixin.java
@@ -0,0 +1,47 @@
+package fuzs.diagonalblocks.fabric.mixin.client;
+
+import fuzs.diagonalblocks.api.v2.impl.StarShapeProvider;
+import fuzs.diagonalblocks.client.util.DestroyEffectsHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WritableLevelData;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLevel.class)
+abstract class ClientLevelFabricMixin extends Level {
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    protected ClientLevelFabricMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+        super(levelData,
+                dimension,
+                registryAccess,
+                dimensionTypeRegistration,
+                isClientSide,
+                isDebug,
+                biomeZoomSeed,
+                maxChainedNeighborUpdates);
+    }
+
+    @Inject(method = "addDestroyBlockEffect", at = @At("HEAD"), cancellable = true)
+    public void addDestroyBlockEffect(BlockPos blockPos, BlockState blockState, CallbackInfo callback) {
+        if (blockState.getBlock() instanceof StarShapeProvider) {
+            if (DestroyEffectsHelper.addDestroyEffects(blockState, this, blockPos, this.minecraft.particleEngine)) {
+                callback.cancel();
+            }
+        }
+    }
+}
